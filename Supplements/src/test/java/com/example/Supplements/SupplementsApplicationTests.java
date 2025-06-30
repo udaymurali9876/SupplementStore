cff57fb09ca76c1171232b06ac84f9a935691853
udaymurali9876 <114414927+udaymurali9876@users.noreply.github.com>
2025-06-30 19:27:48 +0530

    Add files via upload

---
+++ Supplements/src/test/java/com/example/Supplements/SupplementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Supplements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
